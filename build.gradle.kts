64e6767609adf08ae5844b7296b08e4e5e1e3734
Baron Roberts <baron@cthing.com>
2025-10-26 12:28:40 -0700

    Use build greeting plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ plugins {
     jacoco
     `maven-publish`
     signing
+    alias(libs.plugins.buildGreeting)
     alias(libs.plugins.cthingPublishing)
     alias(libs.plugins.cthingVersioning)
     alias(libs.plugins.dependencyAnalysis)
